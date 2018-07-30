8216c7e650b87d7dc737a5a74907025f416c1f81
Mateus Oliveira Souza <mateusoliveirathe@gmail.com>
2018-07-30 20:54:08 -0300

    Maior e menor peso.

--- mundo02/Desafios/Desafio055.py
+++ mundo02/Desafios/Desafio055.py
@@ -1,10 +1,14 @@
 maior = 0
 menor = 0
 for n in range(1,6):
-    peso = int(input('Entre com o seu peso: '))
-    if peso > maior:
+    peso = float(input('Entre com o peso da {}ª pessoa (Kg): '.format(n)))
+    if n == 1:
         maior = peso
-    else:
         menor = peso
-print('O peso maior é {}'.format(maior))
-print('O peso menor é {}'.format(menor))
+    else:
+        if peso > maior:
+            maior = peso
+        if peso < menor:
+            menor = peso
+print('O peso maior é {}Kg'.format(maior))
+print('O peso menor é {}Kg'.format(menor))
