b46fb25f6792248fea258504267fd3356c8c09b4
Mateus Oliveira Souza <mateusoliveirathe@gmail.com>
2018-08-01 10:00:12 -0300

    Aula 14 - WHILE

--- mundo02/Aulas/Aula14.py
+++ mundo02/Aulas/Aula14.py
@@ -1,40 +1,40 @@
 '''enquanto não maçã       while not maçã:
     se chao                 if chao
-        passo
-    se vazio
-        pula
-    se modda
-        pega
-pega'''
+        passo                   passo
+    se vazio                if vazio
+        pula                    pula
+    se moeda                if moeda
+        pega                   pega
+pega                       pega'''
 
-#c =1
-#while c <=10:
-#    print(c)
-#    c += 1
-#print('FIM')
+########### LISTANDO DE 1-10 ###############
+'''c =1
+while c <=10:
+    print(c)
+    c += 1
+print('FIM')'''
 
-##################################3
+
 '''n = 1
 while n != 0:
-    n = int(input('Digite um valor'))
+    n = int(input('Digite um valor: '))
 print('FIM')'''
-##################################3
 
-##################################3
+
 '''r = 'S'
 while r == 'S':
     n = int(input('Digite um valor'))
     r = str(input('Quer continuar? [S/N}')).upper()
 print('FIM')'''
-##################################
 
-n = 1
+
+'''n = 1
 par = impar = 0
 while n != 0:
-    n = int(input('Digite um valor: '))
+    n = int(input('Digite 0(zero) para sair ou Digite um valor: '))
     if n != 0:
         if n % 2 ==0:
             par += 1
         else:
             impar += 1
-print('Você digitou {} numero pares e {} numeros impares'.format(par,impar))
+print('Você digitou {} numero pares e {} numeros impares'.format(par,impar))'''
