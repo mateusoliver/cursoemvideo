a566d4cc64eaccf76afbbdc60067805723c25750
Mateus Oliveira Souza <mateusoliveirathe@gmail.com>
2018-08-01 11:02:19 -0300

    Erro e acerto com máquina!

--- mundo02/Desafios/Desafio058.py
+++ mundo02/Desafios/Desafio058.py
@@ -4,14 +4,13 @@ from random import randint
 cont = 0
 print('O computador esta pensando em um número')
 pcnum = int(randint(0,9))
-time.sleep(4)
+#time.sleep(4)
 eunum = 's'
 while pcnum != eunum:
     eunum = int(input('Tente advinhar o número que o computador pensou. Digite: '))
     if pcnum != eunum:
         cont += 1
+        print('tente novamente!')
     elif pcnum == eunum:
         print('Errou {} vezes, até acertar!!'.format(cont))
         print('O pc pensou no número {} e você disse o número {}.'.format(pcnum,eunum))
-        exit()
-print('tente novamente!')
