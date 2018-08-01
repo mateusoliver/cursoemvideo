13a86f3e9e47bf76f337a047f0f165c6da52cb6e
Mateus Oliveira Souza <mateusoliveirathe@gmail.com>
2018-08-01 10:32:35 -0300

    Exemplo while.

---
+++ mundo02/Desafios/Desafio057.py
@@ -0,0 +1,11 @@
+r = 'S'
+while r == 'S':
+    n = str(input('Digite o seu SEXO [F] ou [M]: ')).upper()
+    if n == 'F':
+        print('Sexo FEMININO')
+    elif n == 'M':
+        print('Sexo MASCULINO')
+    else:
+        print('valor inválido')
+    r = str(input('Quer continuar? [S] para sim!!')).upper()
+print('FIM')
