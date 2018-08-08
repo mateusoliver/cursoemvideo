ce82598a65474529f1a41aba95e9a7e40c8b2dd6
Mateus Oliveira Souza <mateusoliveirathe@gmail.com>
2018-08-08 16:10:55 -0300

    purpurinagem

--- mundo02/Desafios/Desafio67.py
+++ mundo02/Desafios/Desafio67.py
@@ -1,7 +1,9 @@
 while True:
     num = int(input('Quer ver a tabuada de qual valor? '))
+    print('='*40)
     if num < 0:
         print('O PROGRAMA FOI ENCERRADO! VOLTE SEMPRE!')
         break
     for c in range(1,11):
         print(f'{num} * {c} = {num*c}')
+    print('='*40)
