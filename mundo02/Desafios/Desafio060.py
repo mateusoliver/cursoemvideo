0d3c66eb8dd6d802cea43d4c5ee3cfe91c9fa89e
Mateus Oliveira Souza <mateusoliveirathe@gmail.com>
2018-08-07 22:50:37 -0300

    'PA' usando FOR, otimizando código

--- mundo02/Desafios/Desafio060.py
+++ mundo02/Desafios/Desafio060.py
@@ -4,10 +4,15 @@ fatorial = 1
 print('{}! = '.format(n),end='')
 while  n > 0 :
     fatorial = fatorial * n
-    if n > 1:
-        print('{}x'.format(n),end='')
-    elif n == 1:
-        print('{} = '.format(n),end='')
+    print(' {} '.format(n), end='')
+    print('x' if n > 1 else '= ', end='')
     n -= 1
 
 print(fatorial)
+
+############## USANDO METODO DO PYTHON ###################
+'''from math import factorial
+n = int(input('Entre com o valor: '))
+f = factorial(n)
+#print('O fatorial de {} é {}'.format(n,f))
+print(f'O fatorial de {n} é {f}')'''
