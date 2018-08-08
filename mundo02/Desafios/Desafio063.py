aa701e95981bf5e1656099425ab366d1017061c2
Mateus Oliveira Souza <mateusoliveirathe@gmail.com>
2018-08-08 11:28:57 -0300

    Sequência de Fibonacci v1.0

---
+++ mundo02/Desafios/Desafio063.py
@@ -0,0 +1,12 @@
+vez = int(input('Digite quantos termos que exibir: '))
+t1 = 0
+t2 = 1
+print('{} -> {}'.format(t1,t2),end='')
+cont = 3
+while cont <= vez:
+    t3 = t1 + t2
+    print('-> {}'.format(t3),end='')
+    t1 = t2
+    t2 = t3
+    cont += 1
+print(' -> FIM')
