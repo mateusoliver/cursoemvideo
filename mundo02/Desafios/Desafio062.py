fd2a1b9671d37fd690e990655561b098fdc0163c
Mateus Oliveira Souza <mateusoliveirathe@gmail.com>
2018-08-07 23:03:06 -0300

    otimizando cod

--- mundo02/Desafios/Desafio062.py
+++ mundo02/Desafios/Desafio062.py
@@ -1,14 +1,16 @@
 a1 = int(input('Digite o primeiro termo da PA :'))
 r = int(input('Digite a razão:'))
 an = int(input('Digite o ultimo termo que desejas: '))
-
+termos = an
 while an >=1:
     print(a1,end=' -> ')
     a1 = a1 + r
     an -= 1
     if an == 0:
-        an = int(input('\nvoce deseja mostrar mais quantos termos?'))
+        an = int(input('\n^Você deseja mostrar mais quantos termos?'))
+        termos = termos + an
         pass
     if an == 0:
+        print('Voce exibiu {} termos!'.format(termos))
         exit()
 print('ACABOU!!')
