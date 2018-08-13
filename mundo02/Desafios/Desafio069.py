5d1293547c0cae6c5163d6f34ba5a0345f549907
Mateus <mateusoliveirathe@gmail.com>
2018-08-13 20:01:44 -0300

    em andamento

---
+++ mundo02/Desafios/Desafio069.py
@@ -0,0 +1,15 @@
+r = 'S'
+maior = 0
+homens = 0
+mulheres = 0
+while r == 'S':
+    idade = int(input('Entre com sua idade: '))
+    sexo = str(input('Qual seu sexo [M/F]: ')).upper()
+    if idade > 18:
+        maior = maior + 1
+    if sexo == 'M':
+        homens = homens + 1
+    r = str(input('Quer continuar [S/N]')).upper()
+print(f'{idade} {sexo} {r}')
+print(f'{maior} pessoa(s) tem maior que 18 anos')
+print(f'{homens} homens foram cadastrados!')
