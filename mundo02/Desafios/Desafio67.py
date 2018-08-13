b42018529620de94256a1b3f0815e7366cf0df57
Mateus <mateusoliveirathe@gmail.com>
2018-08-13 19:13:07 -0300

    alterações realizadas

--- mundo02/Desafios/Desafio67.py
+++ mundo02/Desafios/Desafio67.py
@@ -2,6 +2,7 @@ while True:
     num = int(input('Quer ver a tabuada de qual valor? '))
     print('='*40)
     if num < 0:
+        print('Valor negativo inserido.')
         print('O PROGRAMA FOI ENCERRADO! VOLTE SEMPRE!')
         break
     for c in range(1,11):
